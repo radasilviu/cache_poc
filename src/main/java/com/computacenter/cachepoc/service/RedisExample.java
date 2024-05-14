6cb6ee73fc8978ec5dc5e73608bb9263b97b37e2
Silviu-nicolae Rada <silviu-nicolae.rada@g4s.com>
2024-05-14 09:04:05 +0300

    redis template example

---
+++ src/main/java/com/computacenter/cachepoc/service/RedisExample.java
@@ -0,0 +1,21 @@
+package com.computacenter.cachepoc.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisExample {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void setValue(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public void get(String key) {
+        redisTemplate.opsForValue().get(key);
+    }
+
+}
